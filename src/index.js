1204c612a25318e85bcb133ccb390a5715c5558c
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-15 14:02:09 +0600

    final update

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ dotenv.config({ path: ".env" });
 
 // This function connects to the database
 connectDB()
-  .than(
+  .then(
     app.listen(process.env.PORT || 3000, () => {
       console.log(`server is running on port ${process.env.PORT || 3000}`);
     })
   )
-  .catch();
+  .catch((error) => {
+    console.log(`server is failed to connect ${error}`);
+  });
