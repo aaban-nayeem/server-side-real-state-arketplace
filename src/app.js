657d5e527e922557cd5386a7aca5d9b22f8cb4ee
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-15 23:49:16 +0600

    Successfully send data from client side

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
+
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/signup", authRouter);
 
 export { app };
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -3,27 +3,12 @@ import User from "../models/user.model.js";
 export const signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
-    // Ensure req.body contains the expected keys and values
-    if (!username || !email || !password) {
-      return res.status(400).json("All fields are required");
-    }
-
-    // Create a new User instance with an object containing username, email, and password
-    const newUser = new User({
-      username,
-      email,
-      password,
-    });
-
-    // Save the new user to the database
+    const newUser = new User({ username, email, password });
     await newUser.save();
-
-    // Respond with a success message
-
-    res.status(201).json("User created successfully!");
+    res.status(201).json("User created successfully");
   } catch (error) {
-    console.error("Error during signup:", error);
-    res.status(500).json("Internal Server Error");
+    console.log(`User create failed ${error}`);
+    res.status(500).json("User create failed");
   }
 };
+// res.status(500).json("User create failed");
